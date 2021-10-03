438372971e6b8dcf6d379bd854e73f559f79656c
aouhman <abdessamad.ouhman@gmail.com>
2021-10-03 23:30:03 +0000

    page not found component

--- src/Pages/page-not-found.tsx
+++ src/Pages/page-not-found.tsx
@@ -7,7 +7,7 @@ const PageNotFound: FunctionComponent = () => {
             <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png" alt="Page non trouvée"/>
             <h1>Hey, cette page n'existe pas !</h1>
             <Link to="/" className="waves-effect waves-teal btn-flat">
-                Retourner à l'accueil
+                Retourner à  l'accueil
             </Link>
         </div>
     );
