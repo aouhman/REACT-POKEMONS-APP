4ec9e33d3d6e25d9efb88409d73373ff5779c9fb
aouhman <abdessamad.ouhman@gmail.com>
2021-10-03 19:55:36 +0000

    Les props + les states

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,56 @@
 import React, {FunctionComponent, useEffect, useState} from 'react';
 import Pokemon from "./models/pokemon";
 import POKEMONS from "./models/mock-pokemon";
+import PokemonList from "./Pages/pokemon-list";
 
-const App: FunctionComponent= () => {
+const App: FunctionComponent = () => {
 
 
     const [name, setName] = useState('REACT');
 
-    const [ListOfPokemon,setListOfPokemon] = useState<Pokemon[]>([]);
+    const [ListOfPokemon, setListOfPokemon] = useState<Pokemon[]>([]);
 
-    const ShowNombrePokemon = () => {
-      console.log(ListOfPokemon.length);
+
+    const addPokemon = (name: any, e: any) => {
+        if (e.nativeEvent.which === 1) {
+            console.log(`le pokémon ${name} a été ajouté au pokédex, avec le clik gauche`)
+        } else if (e.nativeEvent.which === 3) {
+            console.log(`le pokémon ${name} a été ajouté au pokédex, avec le clik droit`)
+        }
     }
-    const addPokemon = (name:any,e:any) => {
-      if(e.nativeEvent.which === 1){
-          console.log(`le pokémon ${name} a été ajouté au pokédex, avec le clik gauche`)
-      }else if(e.nativeEvent.which === 3){
-          console.log(`le pokémon ${name} a été ajouté au pokédex, avec le clik droit`)
-      }
-    }
-    useEffect(()=>{ setListOfPokemon(POKEMONS) },[])
+    useEffect(() => {
+        setListOfPokemon(POKEMONS)
+    }, [])
+
 
     return (
-      <div>
-          {ListOfPokemon.length > 0 ?
-       (
-              <ul>{ListOfPokemon.map(({name}) =>(
-                  <li key={name}>{name}</li>
-              ))}</ul>
-       )
 
-          :
-       (<h1>Bonsoir,  Vous avez de la chance  !</h1>)
-
-
-
-
-      }</div>
-
-
+                <PokemonList/>
 
 
     )
+
+
+    /*
+       return (
+         <div>
+             {ListOfPokemon.length > 0 ?
+          (
+                 <ul>{ListOfPokemon.map(({name}) =>(
+                     <li key={name}>{name}</li>
+                 ))}</ul>
+          )
+
+             :
+          (<h1>Bonsoir,  Vous avez de la chance  !</h1>)
+
+
+
+
+         }</div>
+
+       )
+      */
 
 
 }
