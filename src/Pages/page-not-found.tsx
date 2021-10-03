29c6930b2893c91cac8f231196964770e6672a64
aouhman <abdessamad.ouhman@gmail.com>
2021-10-03 23:26:41 +0000

    page not found component

--- src/Pages/page-not-found.tsx
+++ src/Pages/page-not-found.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const PageNotFound: FunctionComponent = () => {
 
     return (
-        <div className="center">
+        <div className="center">²²²²²
             <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png" alt="Page non trouvée"/>
             <h1>Hey, cette page n'existe pas !</h1>
             <Link to="/" className="waves-effect waves-teal btn-flat">
